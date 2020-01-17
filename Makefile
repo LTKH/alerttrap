7418e38fda44235918ee0ac40c1200bad9dfec2d
Dmitry <dmitry@Jelezka.local>
2020-01-17 18:15:36 +1000

    LoadAlerts

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
-GOPATH=`pwd`/backend
+GOPATH=`pwd`
 
 build:
-	@echo "Building Alertstrap..."
-	@GOPATH=${GOPATH} go build -o backend/bin/alertstrap backend/alertstrap.go
-	@echo "Building Jiramanager..."
-	@GOPATH=${GOPATH} go build -o backend/bin/jiramanager backend/jiramanager.go
+    @echo "Building Alertstrap..."
+    @GOPATH=${GOPATH} go build -o bin/alertstrap alertstrap.go
 
 run:
-	@echo "Runing Alertstrap..."
-	@backend/bin/alertstrap -config backend/conf/config.toml -dbase backend/conf/dbase.sql
+    @echo "Runing Alertstrap..."
+    @go run alertstrap.go -config configs/config.toml
---
+++ alertstrap.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+  "net/http"
+  "time"
+  "log"
+  "os"
+  "os/signal"
+  "syscall"
+  "runtime"
+  "flag"
+  "alertstrap/db"
+  "alertstrap/api"
+  "alertstrap/config"
+)
+
+func main() {
+
+  //limits the number of operating system threads
+  runtime.GOMAXPROCS(runtime.NumCPU())
+
+  //command-line flag parsing
+  cfFile := flag.String("config", "", "config file")
+  //dBase  := flag.String("dbase", "", "sql file")
+  flag.Parse()
+
+  //loading configuration file
+  cfg, err := config.LoadConfigFile(*cfFile)
+  if err != nil {
+    log.Fatalf("[error] %v", err)
+  }
+
+  //connection to data base
+  log.Print("[info] connection to data base")
+  if err := db.ConnectDb(cfg); err != nil {
+    log.Fatalf("[critical] %v", err)
+  }
+
+  //creating data base schema
+  //if *dBase != "" {
+  //  log.Print("[info] creating data base schema")
+  //  if err := db.CreateSchema(*dBase); err != nil {
+  //    log.Fatalf("[critical] %v", err)
+  //  }
+  //}
+
+  //loading hosts table
+  //log.Print("[info] loading hosts from database")
+  //if err := api.LoadHosts(); err != nil {
+  //  log.Fatalf("[critical] %v", err)
+  //}
+
+  //program completion signal processing
+  c := make(chan os.Signal, 2)
+  signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+  go func() {
+    <- c
+    log.Print("[info] alertstrap stopped")
+    os.Exit(0)
+  }()
+
+  //enabled listen port
+  //http.HandleFunc("/get/alerts",  api.GetAlerts)
+  //http.HandleFunc("/get/hosts",   api.GetHosts)
+  //http.HandleFunc("/get/history", api.GetHistory)
+  //http.HandleFunc("/add/alert",   api.AddAlert)
+  go http.ListenAndServe(cfg.Alertstrap.Listen_port, &(api.Api{}))
+  log.Printf("[info] listen port enabled - %s", cfg.Alertstrap.Listen_port)
+
+  log.Print("[info] alertstrap started ^_^")
+
+  //daemon mode
+  for {
+
+    //connection to data base
+    if err := db.ConnectDb(cfg); err != nil {
+      log.Fatalf("[critical] %v", err)
+    }
+
+    //loading alerts
+    if err := api.LoadAlerts(cfg); err != nil {
+      log.Fatalf("[critical] %v", err)
+    }
+
+    //if err := db.ConnectDb(cfg); err != nil {
+    //  log.Printf("[error] %v", err)
+    //  db.ConnectDb(cfg)
+    //} else {
+    //  if err := api.LoadHosts(); err != nil {
+    //    log.Printf("[error] %v", err)
+    //  }
+    //}
+
+    time.Sleep(10 * time.Second)
+
+  }
+}
